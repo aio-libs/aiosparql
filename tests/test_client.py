4e6eec96088f31f6d537058e081c36999ea6f4f8
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-04-25 17:34:51 +0200

    Added tests for the SPARQLClient

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,81 @@
+from aiohttp import web
+import json
+from textwrap import dedent
 import unittest
 
 from aiosparql.client import SPARQLQueryFormatter
+from aiosparql.syntax import IRI, RDF, Triples
+from aiosparql.test_utils import AioSPARQLTestCase, unittest_run_loop
 
 
-class Client(unittest.TestCase):
+async def sparql_endpoint(request):
+    result = {
+        "post": dict((await request.post()).items()),
+        "path": request.path,
+    }
+    return web.Response(text=json.dumps(result),
+                        content_type="application/json")
+
+
+class Client(AioSPARQLTestCase):
+    client_kwargs = {
+        "endpoint": "/sparql",
+        "update_endpoint": "/sparql-update",
+        "graph": IRI("http://mu.semte.ch/test-application"),
+    }
+
+    async def get_application(self):
+        app = web.Application()
+        app.router.add_post('/sparql', sparql_endpoint)
+        app.router.add_post('/sparql-update', sparql_endpoint)
+        return app
+
+    @unittest_run_loop
+    async def test_query(self):
+        triples = Triples([("john", RDF.type, "doe"), ("john", "p", "o")])
+        res = await self.client.query("""
+            SELECT *
+            FROM {{graph}}
+            WHERE {
+                {{}}
+            }
+            """, triples)
+        self.assertEqual(res['path'], self.client_kwargs['endpoint'])
+        self.assertIn('post', res)
+        self.assertIn('query', res['post'])
+        self.assertEqual(res['post']['query'], dedent("""\
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            SELECT *
+            FROM <http://mu.semte.ch/test-application>
+            WHERE {
+                john rdf:type "doe" ;
+                    p "o" .
+            }"""))
+
+    @unittest_run_loop
+    async def test_update(self):
+        triples = Triples([("john", RDF.type, "doe"), ("john", "p", "o")])
+        res = await self.client.update("""
+            WITH {{graph}}
+            INSERT DATA {
+                {{}}
+            }
+            """, triples)
+        self.assertEqual(res['path'], self.client_kwargs['update_endpoint'])
+        self.assertIn('post', res)
+        self.assertIn('update', res['post'])
+        self.assertEqual(res['post']['update'], dedent("""\
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            WITH <http://mu.semte.ch/test-application>
+            INSERT DATA {
+                john rdf:type "doe" ;
+                    p "o" .
+            }"""))
+
+
+class Formatter(unittest.TestCase):
     formatter = SPARQLQueryFormatter()
 
     def _format(self, string, *args, **kwargs):

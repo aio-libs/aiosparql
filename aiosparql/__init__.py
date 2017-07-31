d6646c857775f64758de9e0ec056dcc1c7fc4f79
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-07-31 11:09:17 +0300

    Fix version, setup aiosparql in tox envs

--- aiosparql/__init__.py
+++ aiosparql/__init__.py
@@ -1,2 +1,2 @@
-__version__ = version = "0.10.0a0"
-version_info = tuple([int(d) for d in version.split("-")[0].split(".")])
+__version__ = "0.10.0a0"
+# version_info = tuple([int(d) for d in version.split("-")[0].split(".")])

54d761e0fd8f36cb745c2b2bf131bfea9ed5aeb7
Adam Bannister <adam.p.bannister@gmail.com>
2019-03-27 20:54:11 +0100

    switch to using a pytest fixture from the conftest to access the client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,36 @@
+import aiohttp
+import uuid
+import pytest
+from os import environ as ENV
+
+from aiosparql.client import SPARQLClient
+from aiosparql.test_utils import AioSPARQLTestCase
+from aiosparql.syntax import IRI
+
+
 pytest_plugins = ['aiohttp.pytest_plugin', 'pytester']
+
+
+@pytest.fixture
+async def client(loop):
+    client = SPARQLClient(
+        ENV.get("SPARQL_ENDPOINT", "http://localhost:8890/sparql"),
+        update_endpoint=ENV.get("SPARQL_UPDATE_ENDPOINT"),
+        crud_endpoint=ENV.get(
+            "SPARQL_UPDATE_ENDPOINT",
+            "http://localhost:8890/sparql-graph-crud"
+        ),
+        graph=IRI("http://aiosparql.org/%s" % uuid.uuid4().hex[:7])
+    )
+    yield client
+    try:
+        await client.delete()
+    except aiohttp.ClientResponseError as exc:
+        if exc.code != 404:
+            raise
+    await client.close()
+
+
+@pytest.fixture
+def test_client():
+    return AioSPARQLTestCase()

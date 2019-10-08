4ad72e1ceb2b634b1f191a518ab47c44d6881d86
Adam Bannister <adam.p.bannister@gmail.com>
2019-10-08 20:05:43 +0200

    mention the default graph

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -82,30 +82,31 @@ async def test_update_delete_virtuoso(virtuoso_client):
 
 
 @pytest.mark.asyncio
-async def test_update_delete_jena(jena_client):
+async def test_update_delete_jena(jena_client, truncate_jena):
     update_query = """
     INSERT DATA {
         <#book1> <#price> 42
     }
     """
     await jena_client.update(update_query)
-    delete_query = """
-    DELETE {
-        ?s ?p ?o
-    }
-    WHERE {
-        ?s ?p ?o
-    }
-    """
-    result = await jena_client.update(delete_query)
-    print(type(result))
-    print(dir(result))
-    print(result)
     select_query = """
     SELECT *
     WHERE {
+      GRAPH <urn:x-arq:DefaultGraph> {
         ?s ?p ?o
+      }
     }
     """
+    result = await jena_client.query(select_query)
+    assert len(result['results']['bindings']) == 1
+    delete_query = """
+    DELETE
+    WHERE {
+      GRAPH <urn:x-arq:DefaultGraph> {
+        ?s ?p ?o
+      }
+    }
+    """
+    result = await jena_client.update(delete_query)
     result = await jena_client.query(select_query)
     assert len(result['results']['bindings']) == 0
